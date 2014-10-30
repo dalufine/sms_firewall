9895a58ca330455b178f224aaa8f1411bf4eafd2
malyanov <malyanov.dmitry@gmail.com>
2014-10-30 18:52:33 +0300

    settings activity

--- src/com/quazar/sms_firewall/activities/SettingsActivity.java
+++ src/com/quazar/sms_firewall/activities/SettingsActivity.java
@@ -3,11 +3,16 @@ package com.quazar.sms_firewall.activities;
 import com.quazar.sms_firewall.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SettingsActivity extends Activity{
+public class SettingsActivity extends BaseActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState){	
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+	}
+	
+	public void onSave(View v){
+		
 	}
 }
